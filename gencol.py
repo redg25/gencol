5ec8bb2b54091bd2676d04a2d56489670d911cf4
regis <regiscorblin@yahoo.fr>
2021-10-18 21:52:25 +0200

    Initial commit

--- gencol.py
+++ gencol.py
@@ -2,7 +2,16 @@ from os import listdir
 import os
 from os.path import isfile, join
 from PIL import Image
-print(os.path)
-folder = "images"
-bg_folder = f'{folder}/backgrounds/)'
-onlyfiles = [f for f in listdir(bg_folder) if isfile(join(bg_folder, f))]
+
+print(os.getcwd())
+
+path = 'C:\\Users\\regis\\PycharmProjects\\gencol\\images'
+
+dic_images ={}
+subfolders =  [(f.path,f.name) for f in os.scandir(path) if f.is_dir() ]
+print(subfolders)
+for f in subfolders:
+    my_path = os.path.join(path, f[1])
+    onlyfiles = [f for f in listdir(my_path) if isfile(join(my_path, f))]
+    dic_images[f[1]] = onlyfiles
+print(dic_images)
