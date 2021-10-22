7c5e1fc912e5c4087a1c41c07b08e7c3c2ed8f67
regis <regiscorblin@yahoo.fr>
2021-10-22 15:11:41 +0200

    Work on setting feature single position

--- gencol.py
+++ gencol.py
@@ -1,30 +1,22 @@
-from os import listdir
 import os
-import re
-from os.path import isfile, join
-from PIL import Image
-
 
 class gencol():
 
     def __init__(self,path):
         self.path = path
-        self.all_features = []
+        self.all_features = {}
 
     def get_content(self):
-        self.all_features = [features(f.path,f.name) for f in os.scandir(self.path) if f.is_dir()]
-        for i, folder in enumerate(self.all_features):
-            folder.order = i+1
+        self.all_features = dict((f.name, features(f.name, f.path)) for f in os.scandir(self.path) if f.is_dir())
+        for i, folder in enumerate(self.all_features.values()):
+            folder._order = i+1
             folder.project = self
 
-
-
-
-
-    def set_f_order(self,f_name,pos):
-        pass
-
-
+    def featurePos(self,name:str,pos:int):
+        if name in self.all_features:
+            self.all_features[name].order = pos
+        else:
+            raise Exception(f"{name} is not a folder in {self.path}")
 
 class features():
 
@@ -32,12 +24,27 @@ class features():
         self.name = name
         self.path = path
         self.mandatory = True
-        self.order = None
+        self._order = None
         self.project = None
+        self.all_images = {}
 
+    @property
+    def order(self):
+        """The order property."""
+        return self._order
 
-    def position(self,order):
-        self.order =  order
+    @order.setter
+    def order(self, value):
+        if value < len(self.project.all_features):
+            self._order = value
+        else:
+            raise Exception(f"The position of the feature must be between"\
+                            f" the range of features: {len(self.project.all_features)}")
+
+    @order.deleter
+    def radius(self):
+        print("Delete radius")
+        del self._order
 
 
 
@@ -45,4 +52,8 @@ test = gencol('C:\\Users\\regis\\PycharmProjects\\gencol\\images')
 test.get_content()
 print (test.all_features)
 for feature in test.all_features:
-    print(feature.name, feature.order)
+    print(feature, test.all_features[feature].order)
+test.featureOrder('background',2)
+for feature in test.all_features:
+    print(feature, test.all_features[feature].order)
+
