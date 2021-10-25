09b0fd520f5a7cc5f31f346d3e6b64e37107ea39
regis <regiscorblin@yahoo.fr>
2021-10-25 16:20:27 +0200

    Some PEP8 cleaning

--- gencol.py
+++ gencol.py
@@ -1,58 +1,64 @@
 import os
 import json
 
-class gencol():
 
-    def __init__(self,path):
+class Gencol:
+    """Class representing a collection generator project.
+    Params:
+        - path: The path of the project folder
+    """
+    def __init__(self, path):
         self.path = path
+        # Equals the last sub folder name in the path
         self.name = self.path.split('\\')[-1]
+        # Represents all the sub folders in the project folder
         self.all_features = {}
-        self.pjson = None
+        self.json = None
 
     def get_content(self):
-        '''
-        Creates features and Image object based on the path given in the gencol object.
-        All features are the list of all subfolders
-        All Image are the images in those subfolders
-        :return:
-        '''
-        self.all_features = dict((f.name, features(f.name, f.path,self)) for f in os.scandir(self.path) if f.is_dir())
+        """Creates features and Image object based on the path given in the Gencol object.
+        """
+        self.all_features = dict((feature.name, Feature(feature.name, feature.path, self))
+                                 for feature in os.scandir(self.path) if feature.is_dir())
+
         for i, folder in enumerate(self.all_features.values()):
-            folder._order = i+1 #Set the feature.order based on system folder sorting
+            folder._order = i+1  # Set the feature.order based on system folder sorting
             folder.all_images = dict((img.name, Image(img.name, img.path, folder))
                                      for img in os.scandir(folder.path) if not img.is_dir())
 
-    def feature_pos(self,name:str,pos:int):
+    def feature_pos(self, name: str, pos: int):
         if name in self.all_features:
             self.all_features[name].order = pos
         else:
             raise Exception(f"{name} is not a folder in {self.path}")
 
-
     def get_json(self):
-        '''
-        Create a json object with the structure of the project and the attributes of each element.
-        :return:
-        '''
+        """Create a json object with the structure of the project and the attributes of each element"""
         project_dict = {}
-        for key, value in self.all_features.items():
-            images = dict((x,{'path': y.path, 'rarity': y.rarity}) for x, y in value.all_images.items())
-            project_dict[key]={'path': value.path,
-                               'mandatory': value.mandatory,
-                               'position': value.order,
-                               'images': images}
-        self.pjson = json.dumps(project_dict, indent=4)
+        for feature, feature_instance in self.all_features.items():
+            # For a feature, to create a dictionary with image names as keys
+            # and their attributes as a dictionary for values
+            images = dict((img, {'path': img_instance.path,
+                                 'rarity': img_instance.rarity})
+                          for img, img_instance in feature_instance.all_images.items())
+            # To add a feature as a key in the project_dict
+            # and its attributes as a dictionary for values.
+            # The value for the images attribute is the "images" dictionary
+            project_dict[feature] = {'path': feature_instance.path,
+                                     'mandatory': feature_instance.mandatory,
+                                     'position': feature_instance.order,
+                                     'images': images}
+
+        self.json = json.dumps(project_dict, indent=4)
 
 
+class Feature:
 
-
-class features():
-
-    def __init__(self,name,path,project):
+    def __init__(self, name, path, project):
         self.name = name
         self.path = path
-        self.mandatory = True
-        self._order = None
+        self.mandatory = True  # Is this feature required in all generated images?
+        self._order = None  # Order of the feature when overlaid with other features
         self.project = project
         self.all_images = {}
 
@@ -74,7 +80,8 @@ class features():
         print("Delete radius")
         del self._order
 
-class Image():
+
+class Image:
 
     def __init__(self, name, path, feature):
         self.name = name
@@ -82,7 +89,7 @@ class Image():
         self.rarity = 100
         self.feature = feature
 
-test = gencol('C:\\Users\\regis\\PycharmProjects\\gencol\\images')
+test = Gencol('C:\\Users\\regis\\PycharmProjects\\gencol\\images')
 test.get_content()
 test.get_json()
 with open('test.json','w') as outfile:
